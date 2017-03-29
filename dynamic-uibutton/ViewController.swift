e2b7b042982b0ec772e3b15c69c3bb39460a1123
Hafiz Wahid <hafizwahid@winapp.my>
2017-03-29 12:53:54 +0800

    Initial Commit

--- dynamic-uibutton/ViewController.swift
+++ dynamic-uibutton/ViewController.swift
@@ -10,9 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aScrollView: UIScrollView!
+    
+    let gapBetweenButtons: CGFloat = 10
+    let buttonCount = 3
+    var aButton = UIButton()
+    var xCoord: CGFloat = 10
+    var buttonHeight: CGFloat?
+    var buttonWidth: CGFloat?
+    var margin: CGFloat?
+    var centerY: CGFloat?
+    var buttonCenterY: CGFloat?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        margin = CGFloat(0.3 * aScrollView.frame.height)
+        buttonHeight = aScrollView.frame.height - margin!
+        buttonWidth = buttonHeight
+        
+        centerY = aScrollView.frame.size.height/2
+        buttonCenterY = centerY! - buttonHeight!/2
+        
+        
+        var itemCount = 0
+        
+        for i in 0..<buttonCount {
+            
+            itemCount = i
+            
+            aButton = UIButton(type: .custom)
+            
+            //this
+            aButton.frame = CGRect(x: xCoord, y: buttonCenterY!, width: buttonWidth!, height: buttonHeight!)
+            
+            aButton.backgroundColor = UIColor.blue
+            aButton.layer.cornerRadius = aButton.frame.size.width / 2
+            aButton.clipsToBounds = true
+            
+            xCoord += buttonWidth! + gapBetweenButtons
+            
+            aScrollView.addSubview(aButton)
+            
+        }
+        //this
+        let totalWidth = ((buttonWidth! + gapBetweenButtons) * CGFloat(buttonCount)) + 5
+        
+        //this
+        aScrollView.contentSize = CGSize(width: totalWidth, height: aScrollView.frame.height)
+        
     }
 
     override func didReceiveMemoryWarning() {
