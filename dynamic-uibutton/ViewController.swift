2905d42284d82a9bbf0651015affeff3eaa30e7b
Hafiz Wahid <hafizwahid@winapp.my>
2017-03-29 12:47:01 +0800

    Initial Commit

---
+++ dynamic-uibutton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamic-uibutton
+//
+//  Created by Hafiz Wahid on 29/03/2017.
+//  Copyright © 2017 hw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
